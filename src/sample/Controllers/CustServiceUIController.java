b04c101d7e76670829be7e696721b150d5f64b82
LearLocker <b.xenia.d-97@yandex.ru>
2018-12-20 20:58:20 +0700

    Update CustServiceUIController.java

--- src/sample/Controllers/CustServiceUIController.java
+++ src/sample/Controllers/CustServiceUIController.java
@@ -207,7 +207,7 @@ public class CustServiceUIController {
             ResultSet rs = null;
             rs = Main.getStmt().executeQuery(Select.getPath);
             while (rs != null && rs.next()) {
-                AllSerPath.add(rs.getString(Select.DataSerPath));
+                AllSerPath.add(rs.getString(Select.dataPathSER));
             }
         } catch (SQLException e) {
             e.printStackTrace();
